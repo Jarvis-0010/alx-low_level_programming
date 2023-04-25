c79b63caa04f1f4e3a2a119f1fbbae503640a3fb
Jarvis-0010 <jo.mwangi0010@gmail.com>
2023-04-25 04:04:10 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,6 +9,10 @@ int main(void)
 	print_alphabet(void);
 	return (0);
 }
+/**
+ * print_alphabet - Entry point
+ * Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
