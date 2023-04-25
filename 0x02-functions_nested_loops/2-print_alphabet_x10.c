c30a5223779d15bb7dde409fa9e3a8347958b99c
Jarvis-0010 <jo.mwangi0010@gmail.com>
2023-04-25 04:25:04 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			putchar(alphabet[j]);
+			putchar('\n');
+		}
+	}
+
+
+}
