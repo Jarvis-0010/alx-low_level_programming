8c7350fae4526b772b80adaf21521b50a0bea662
Jarvis-0010 <jo.mwangi0010@gmail.com>
2023-04-28 14:42:04 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,18 @@ void print_most_numbers(void)
 
 	for (x = '0'; x <= '9'; x++)
 	{
-		if (x != '2' || x != '4')
+		if (x == '2')
+		{
+			continue;
+		}
+		else if (x == '4')
+		{
+			continue;
+		}
+		else
+		{
 			putchar(x);
+		}
 	}
 	putchar('\n');
 }
