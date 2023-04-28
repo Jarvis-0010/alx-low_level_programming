662adc5d1d7ab59d424483a9b1183ed46c2ed922
Jarvis-0010 <jo.mwangi0010@gmail.com>
2023-04-28 15:58:55 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: value being tested
+ * Return: Always 0 (Success)
+*/
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for ( i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - Entry point
+ * @n: value being tested
+ * Return: Always 0 (Success)
+*/
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
