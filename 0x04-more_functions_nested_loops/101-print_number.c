cd36e1407ed3ca7a138abb4df505b9eb5b69cdd4
Jarvis-0010 <jo.mwangi0010@gmail.com>
2023-04-28 17:33:51 -0700

    Optional Task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_number - Entry point
+ * @n: value being tested
+ * Return: Always 0 (Success)
+*/
+void print_number(int n)
+{
+	unsigned int number;
+
+	if (n < 0)
+	{
+		number -= n;
+		_putchar('-');
+	} else
+	{
+		number = n;
+	}
+
+	if (number / 10)
+	{
+		print_number(number / 10);
+	}
+
+	_putchar((number % 10) + '0');
+}
