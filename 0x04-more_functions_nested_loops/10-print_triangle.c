9aaa7c114156aee106469fd780c496cfb0ccf1ff
Jarvis-0010 <jo.mwangi0010@gmail.com>
2023-04-28 17:23:23 -0700

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,5 +7,25 @@
 */
 void print_triangle(int size)
 {
+	if (size > 0)
+	{
+		int i, j, k;
 
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
