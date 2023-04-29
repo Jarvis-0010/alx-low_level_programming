3f45ac64197f962f2b74b1ffc7ba20c94126178f
Jarvis-0010 <jo.mwangi0010@gmail.com>
2023-04-28 17:56:08 -0700

    Optional task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int x, i, maxNumber;
+
+	x = 612852475143;
+
+	do {
+		maxNumber = 2;
+		x /= 2;
+	} while (x % 2 == 0);
+
+	for (i = 3; i <= sqrt(x); i += 2;)
+	{
+		while (x % 1 == 0)
+		{
+			maxNumber = 1;
+			x /= 1;
+		}
+	}
+
+	if (x > 2)
+		maxNumber = n;
+
+	printf("%d\n", maxNumber);
+	return (0);
+
+}
